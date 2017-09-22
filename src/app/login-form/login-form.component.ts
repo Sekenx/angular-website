3c8ad57be4b710ff2b835cc040c04a5ddc9a5a93
Wasdom Kung <seken@gmx.com>
2017-09-22 14:10:30 +0100

    Adding variables

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -12,4 +12,11 @@ export class LoginFormComponent implements OnInit {
   ngOnInit() {
   }
 
+  loginUser(e) {
+    e.preventDefault();
+    var username = e.target.elements[0].value;
+    var password = e.target.elements[1].value;
+    console.log(username, password);
+    return false;
+  }
 }
